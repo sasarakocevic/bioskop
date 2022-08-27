b76bc90f1564c01937860ce3bbf6bd93a0425c59
Luka Kastratović <lukakuster1@gmail.com>
2022-08-27 22:13:34 +0200

    add routes

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -21,6 +21,34 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group([
     'middleware' => 'api'
 ], function ($router) {
-    Route::get( 'sala', [\App\Http\Controllers\SalaController::class, 'index']);
-    Route::post( 'sala', [\App\Http\Controllers\SalaController::class, 'store']);
+
+    //GLEDALAC
+    Route::get('gledalac', [\App\Http\Controllers\GledalacController::class, 'index']);
+    Route::get('gledalac/{id}', [\App\Http\Controllers\GledalacController::class, 'show']);
+
+    //SALA
+    Route::get('sala', [\App\Http\Controllers\SalaController::class, 'index']);
+    Route::get('sala/{id}', [\App\Http\Controllers\SalaController::class, 'show']);
+    Route::post('sala', [\App\Http\Controllers\SalaController::class, 'store']);
+    Route::put('sala/{id}', [\App\Http\Controllers\SalaController::class, 'update']);
+    Route::delete('sala/{id}', [\App\Http\Controllers\SalaController::class, 'destroy']);
+
+    //FILM
+    Route::get('film', [\App\Http\Controllers\FilmController::class, 'index']);
+    Route::get('film/{id}', [\App\Http\Controllers\FilmController::class, 'show']);
+    Route::post('film', [\App\Http\Controllers\FilmController::class, 'store']);
+    Route::put('film/{id}', [\App\Http\Controllers\FilmController::class, 'update']);
+    Route::delete('film/{id}', [\App\Http\Controllers\FilmController::class, 'destroy']);
+
+    //SLIKA
+    Route::get('slika', \App\Http\Controllers\SlikaController::class, 'index');
+    Route::get('slika/{id}', [\App\Http\Controllers\SlikaController::class, 'show']);
+
+    //REZERVACIJA
+    Route::resource('rezervacija', \App\Http\Controllers\RezervacijaController::class);
+
+    //PROJEKCIJA FILMA
+    Route::resource('projekcija_filma', \App\Models\ProjekcijaFilma::class);
+    
+
 });
